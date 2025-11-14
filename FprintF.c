1f31aeb5d5e8db08f491391bc181435b5ec6cc78
martingeronimo8370-cpu <martin.geronimo8370@alumnos.udg.mx>
2025-11-14 10:32:55 -0600

    Add files via upload

--- FprintF.c
+++ FprintF.c
@@ -2,12 +2,17 @@
 
 int main(){
 	FILE *f = fopen(" salida.txt", "w");
+	char linea[200];
+	
 	if(f == NULL){
 		printf("ERROR al abrir el archivo.\n");
 		return 1;
 	}
-	fprintf(f, "hola mundo. \n");
-	fprintf(f, "este archivo fue creado desde C.\n");
+	
+	while(fgets(linea, 200, f) != NULL)
+	printf("%s", linea);
+//	fprintf(f, "hola mundo. \n");
+//	fprintf(f, "este archivo fue creado desde C.\n");
 	
 	fclose(f);
 	return 0;
